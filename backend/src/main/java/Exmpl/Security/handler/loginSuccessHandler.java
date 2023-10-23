bafbe1c6a1c554bf89698963d77fc690dcdd224a
EkingDoral <937858146@qq.com>
2023-10-23 11:08:55 +0800

    封装了jwts工具类，完成了token认证

--- backend/src/main/java/Exmpl/Security/handler/loginSuccessHandler.java
+++ backend/src/main/java/Exmpl/Security/handler/loginSuccessHandler.java
@@ -2,6 +2,8 @@ package Exmpl.Security.handler;
 
 import Exmpl.Entity.User;
 import Exmpl.utils.jwtUtils;
+import Exmpl.utils.loginResult;
+import Exmpl.utils.resultCode;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import javax.annotation.Resource;
@@ -9,6 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -26,13 +30,22 @@ public class loginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setContentType("application/json;charset=UTF-8");
         User user = (User) authentication.getPrincipal();
+
         //生成token
-        String token = jwtUtils.genarateToken(user);
+        String token = jwtUtils.generateToken(user);
+
         //设置token的签名密钥和过期时间
+        long  expiresTime = Jwts.parserBuilder()
+                .setSigningKey(jwtUtils.getSecret())
+                .build()
+                .parseClaimsJws(token.replace("jwt_", ""))
+                .getBody().getExpiration().getTime();
 
+        //创建login带令牌的返回对象
+        loginResult loginResult = new loginResult(user.getUno(), resultCode.SUCCESS, token, expiresTime);
 
-
-        String result = JSON.toJSONString(user, SerializerFeature.DisableCircularReferenceDetect);
+        //返回JSON
+        String result = JSON.toJSONString(loginResult, SerializerFeature.DisableCircularReferenceDetect);
         ServletOutputStream outputStream = response.getOutputStream();
         outputStream.write(result.getBytes(StandardCharsets.UTF_8));
         outputStream.flush();
--- backend/src/main/java/Exmpl/utils/jwtUtils.java
+++ backend/src/main/java/Exmpl/utils/jwtUtils.java
@@ -1,4 +1,87 @@
 package Exmpl.utils;
 
+import Exmpl.Entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "myjwt")
 public class jwtUtils {
+    private String secret; //密钥
+    private long expires; //过期时间
+
+    //从数据声明生产令牌
+    private String generateToken(Map<String,Object> claims) {
+        Date expirationDate = new Date(System.currentTimeMillis() + expires);
+        return Jwts.builder().setClaims(claims).setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+
+    }
+
+    //从令牌中获取数据声明
+    public Claims getClaims(String token){
+        Claims claims;
+        try{
+            claims = Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token).getBody();
+        } catch (Exception e){
+            claims = null;
+        }
+        return claims;
+    }
+
+    //生成令牌
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> myclaims = new HashMap<>(2);
+        myclaims.put(Claims.SUBJECT, userDetails.getUsername());
+        myclaims.put(Claims.ISSUED_AT, new Date());
+        return generateToken(myclaims);
+    }
+
+    //从令牌中获取用户名
+    public String getUsernameFromToken(String token){
+        String username;
+        try {
+            Claims claims = getClaims(token);
+            username = claims.getSubject();
+        } catch (Exception e){
+            username = null;
+        }
+        return username;
+    }
+
+    //判断令牌是否过期
+    public boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    //刷新令牌
+    public String reToken(String token) {
+        String reToken;
+        try {
+            Claims claims = getClaims(token);
+            claims.put(Claims.ISSUED_AT, new Date());
+            reToken = generateToken(claims);
+        } catch (Exception e) {
+            reToken = null;
+        }
+        return reToken;
+    }
+
+    //验证令牌
+    public Boolean validateToken(String token,  UserDetails userDetails) {
+        User user = (User) userDetails;
+        String username = getUsernameFromToken(token);
+        return (username.equals(user.getUsername())&&isTokenExpired(token));
+    }
 }
+
